34bd1b971615d72bad51fd892fbd7096e76510fc
Vishrut Reddi <vishrutreddi@gmail.com>
2017-10-04 00:49:53 -0700

    Add bulk update json directory script

--- Scripts/BulkJsonDirectoryUpdate.ps1
+++ Scripts/BulkJsonDirectoryUpdate.ps1
@@ -34,17 +34,20 @@ param(
     [Parameter(Mandatory=$true, Position=4, HelpMessage="Specifies the name of the collection in the mongo database.")]
     [string]$Collection,
 
-    [Parameter(Mandatory=$true, Position=5, HelpMessage="Specifies a username with which to authenticate to a MongoDB database that uses authentication.")]
+    [Parameter(Mandatory=$true, Position=5, HelpMessage="Modifies the import process to update existing objects in the database if they match an imported object, while inserting all other objects")]
+    [string]$UpsertKey,
+
+    [Parameter(Mandatory=$true, Position=6, HelpMessage="Specifies a username with which to authenticate to a MongoDB database that uses authentication.")]
     [string]$Username,
 
-    [Parameter(Mandatory=$true, Position=6, HelpMessage="Specifies a password with which to authenticate to a MongoDB database that uses authentication.")]
+    [Parameter(Mandatory=$true, Position=7, HelpMessage="Specifies a password with which to authenticate to a MongoDB database that uses authentication.")]
     [string]$Password
 )
 
 $files = Get-ChildItem $Path | select Name, Extension, Directory;
 
 Write-Host "";
-Write-Host "Starting Imports..." -foregroundcolor "cyan";
+Write-Host "Starting Update Imports..." -foregroundcolor "cyan";
 
 ForEach($file in $files) {
     if($file.Extension -eq ".json") {
@@ -57,11 +60,11 @@ ForEach($file in $files) {
         Try {
             for ($i = 1; $i -le 100; $i++)
             {
-                write-progress -activity "Importing $fileName" -status "$i% Complete:" -percentcomplete $i;
+                write-progress -activity "Updating $fileName" -status "$i% Complete:" -percentcomplete $i;
                 Start-Sleep -Milliseconds 10;
             }
 
-            mongoimport -h $HostUrl --port $Port -d $Database -c $Collection -u $Username -p $Password --type json --file $filePath;
+            mongoimport -h $HostUrl --port $Port -d $Database -c $Collection --upsert --upsertFields $UpsertKey -u $Username -p $Password --type json --file $filePath;
         }
         Catch {
             $errorOccured = $TRUE;
@@ -71,7 +74,7 @@ ForEach($file in $files) {
             Write-Host $fileName, "       [ ERROR ]" -foregroundcolor "red";
         }
         else {
-            Write-Host $fileName, "       [ IMPORTED ]" -foregroundcolor "green";
+            Write-Host $fileName, "       [ UPDATED ]" -foregroundcolor "green";
         }
     }
 }
